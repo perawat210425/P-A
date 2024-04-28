c1c04aab1783d159092ae784fe643d28788c875f
perawat210425 <149926414+perawat210425@users.noreply.github.com>
2024-04-28 11:57:03 +0700

    Gameover

---
+++ Assets/Code/Gameover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void PlayGame1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
